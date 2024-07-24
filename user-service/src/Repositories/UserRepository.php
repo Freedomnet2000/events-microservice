fe4adb08e6823b0aeef2b1772bd4c317adb0154f
Nir Home <nirfridm@gmail.com>
2024-07-24 13:16:41 +0300

    write user to json & reorganize event structure

--- user-service/src/Repositories/UserRepository.php
+++ user-service/src/Repositories/UserRepository.php
@@ -3,6 +3,24 @@
 namespace App\Repositories;
 
 class UserRepository {
+    private $filePath;
+
+    public function __construct() {
+        $this->filePath = __DIR__ . '/../../data/users.json';
+    }
+
     public function save($data) {
+        $users = $this->readData();
+        $users[] = $data;
+        file_put_contents($this->filePath, json_encode($users, JSON_PRETTY_PRINT));
+    }
+
+    private function readData() {
+        if (!file_exists($this->filePath)) {
+            return [];
+        }
+
+        $json = file_get_contents($this->filePath);
+        return json_decode($json, true);
     }
 }
--- user-service/src/Utils/EventPublisher.php
+++ user-service/src/Utils/EventPublisher.php
@@ -5,7 +5,7 @@ namespace App\Utils;
 class EventPublisher {
     public function publish($eventType, $data) {
         $event = ['type' => $eventType, 'data' => $data];
-        file_get_contents('http://event-service/index.php', false, stream_context_create([
+        file_get_contents('http://localhost:8002/index.php', false, stream_context_create([
             'http' => [
                 'method' => 'POST',
                 'header' => 'Content-Type: application/json',
